3382f8910fc4b328bcdb02e9ece8af402a886cfb
afifao1 <ruhsonaholmirzaeva85@gmail.com>
2025-02-05 12:23:31 +0500

    feat: change design form control

--- create.php
+++ create.php
@@ -17,7 +17,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Add new post</title>
+    <style>
+   h1{
+    color:red;
+    text-align:center
+   }
+   form{
+    width: 50%;
+    margin: 0 auto;
+   }
+   input{
+    width: 100%;
+    height:30px;
+    border:1px solid red;
+    border-radius: 6px;
+    padding-left:10px;
+
+   }
+   
+   textarea{
+    width: 100%;
+    border:1px solid red;
+    border-radius: 6px;
+    padding-left:10px;
+    margin: 20px 0;
+   }
+   button{
+    width: 150px;
+    height:35px;
+    background-color: red;
+    color: white;
+   }
+    </style>
 </head>
+
 <body>
     <h1>Add new post</h1>
     <form method="post">
